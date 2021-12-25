b95a391bb6d60fa36162e2b5ecb480e447bd06f7
Mark Doyle <markdoyle222@gmail.com>
2021-12-25 04:27:59 +0000

    add initial migration (CreateFlights)

---
+++ Vatly.Api/Data/Migrations/20211225042322_CreateFlights.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vatly.Api.Migrations
+{
+    public partial class CreateFlights : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Callsign = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    Longitude = table.Column<double>(type: "double precision", nullable: false),
+                    Altitude = table.Column<double>(type: "double precision", nullable: false),
+                    Heading = table.Column<int>(type: "integer", nullable: false),
+                    OriginIcao = table.Column<string>(type: "text", nullable: true),
+                    DestinationIcao = table.Column<string>(type: "text", nullable: true),
+                    Route = table.Column<string>(type: "text", nullable: true),
+                    AircraftType = table.Column<string>(type: "text", nullable: true),
+                    PlannedAltitude = table.Column<double>(type: "double precision", nullable: true),
+                    Alternate = table.Column<string>(type: "text", nullable: true),
+                    CruiseSpeed = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+        }
+    }
+}
