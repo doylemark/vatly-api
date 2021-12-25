a8562184300512c25bd60f343f85d7ca1e32fef0
Mark Doyle <markdoyle222@gmail.com>
2021-12-25 06:56:41 +0000

    disable hangfire logging

--- Vatly.Api/Program.cs
+++ Vatly.Api/Program.cs
@@ -5,14 +5,11 @@ using Vatly.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddEntityFrameworkNpgsql();
+
 builder.Services.AddDbContext<ApplicationDbContext>((sp, opt) =>
 {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("ApplicationDb"), acts =>
@@ -27,16 +24,19 @@ builder.Services.AddHangfire(configuration =>
 });
 builder.Services.AddHangfireServer();
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(opts =>
+    app.UseSwaggerUI(opt =>
     {
-        opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        opts.RoutePrefix = string.Empty;
+        opt.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        opt.RoutePrefix = string.Empty;
 
     });
     app.UseHangfireDashboard("/jobs");
